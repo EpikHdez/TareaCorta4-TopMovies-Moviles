198ca5a3ba8e4303e1cd28686534e6ab1a19bf55
Unknown <erick.hdez06@outlook.com>
2018-03-19 12:30:34 -0600

    Adding information for user

--- TopMovies/app/src/main/java/erickhdez/com/topmovies/MainActivity.java
+++ TopMovies/app/src/main/java/erickhdez/com/topmovies/MainActivity.java
@@ -1,11 +1,17 @@
 package erickhdez.com.topmovies;
 
+import android.app.Activity;
+import android.app.Application;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
+import android.webkit.WebView;
 import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -13,30 +19,79 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements IReceiveAsyncTaskResponse {
+    private TextView txtLoading;
+    private Timer loadingTimer;
+    private IReceiveAsyncTaskResponse taskRequester;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        WebView webView = findViewById(R.id.webViewContainer);
+        webView.loadUrl("file:///android_asset/index.html");
+
         RelativeLayout overLayout = findViewById(R.id.OverLayout);
         overLayout.bringToFront();
         overLayout.setVisibility(View.VISIBLE);
+
+        txtLoading = findViewById(R.id.txtLoading);
+        loadingTimer = new Timer();
+        taskRequester = this;
 
         requestsMovies();
     }
 
     private void requestsMovies() {
-        String url = "http://www.imdb.com/list/ls064079588/";
-        String wantedElements = "20";
+        final String url = "http://www.imdb.com/list/ls064079588/";
+        final String wantedElements = "20";
 
-        new RetriveMovies(this).execute(url, wantedElements);
+        loadingTimer.scheduleAtFixedRate(new TimerTask() {
+            int[] loadingTexts = {R.string.loading_1, R.string.loading_2, R.string.loading_3};
+            int textsCount = loadingTexts.length;
+            boolean retrieving = false;
+            int index = 0, attemps = 0;
+
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        txtLoading.setText(loadingTexts[index++]);
+
+                        if(attemps == 60) {
+                            Toast.makeText(getApplicationContext(), R.string.no_connection, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+                index %= textsCount;
+                attemps++;
+
+                if(isOnline() && !retrieving) {
+                    retrieving = true;
+                    new RetriveMovies(taskRequester).execute(url, wantedElements);
+                }
+            }
+        }, 0, 1000);
+
+
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
     }
 
     @Override
@@ -49,5 +104,6 @@ public class MainActivity extends AppCompatActivity implements IReceiveAsyncTask
 
         RelativeLayout overLayout = findViewById(R.id.OverLayout);
         overLayout.setVisibility(View.INVISIBLE);
+        loadingTimer.cancel();
     }
 }
